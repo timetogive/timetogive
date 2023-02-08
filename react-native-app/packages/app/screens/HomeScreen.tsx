4b2f98d56cbd5ba2d089dfa4b0137c33fd664680
hoochani <james.allchin+dev@gmail.com>
2023-02-08 11:31:22 +0000

    Attemted to add mapbox which needed native code outside of Expo SDK / Expo Go.
    Leaving a commit here - but reverting back to using expo MapView instead

--- react-native-app/packages/app/screens/HomeScreen.tsx
+++ react-native-app/packages/app/screens/HomeScreen.tsx
@@ -1,7 +1,34 @@
 import { ScreenWrapper, Map } from 'app/components'
 import React, { useState } from 'react'
-import { View, Text } from 'react-native'
+import { StyleSheet, View } from 'react-native'
+import MapboxGL from '@rnmapbox/maps'
 
-export default function HomeScreen() {
-  return <ScreenWrapper></ScreenWrapper>
+// This is the public token not the secret token (the secret token is used by the build in EAS)
+MapboxGL.setAccessToken(
+  'pk.eyJ1IjoiaG9vY2hhbmkiLCJhIjoiY2xkdW53MTJvMDdxbzNwbXZpYm9lMGZjciJ9.9CGrkAowMwBEiFcPabQ_kQ'
+)
+
+export const HomeScreen = () => {
+  return (
+    <View style={styles.page}>
+      <View style={styles.container}>
+        <MapboxGL.MapView style={styles.map} />
+      </View>
+    </View>
+  )
 }
+
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  container: {
+    height: 300,
+    width: 300,
+  },
+  map: {
+    flex: 1,
+  },
+})
