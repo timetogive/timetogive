f6b4a524f43401b67aaefec65db198b80140d943
hoochani <james.allchin+dev@gmail.com>
2023-04-02 07:16:36 +0100

    On sign up, set that the ts and cs have been accepted

--- supabase/setup-scripts/functions/handle_new_user.sql
+++ supabase/setup-scripts/functions/handle_new_user.sql
@@ -7,6 +7,7 @@ begin
   insert into public.prefs (id) values (new.id);
   insert into public.profiles (id, full_name)
   values (new.id, new.raw_user_meta_data->>'full_name');
+  insert into public.tc_agreements (user_id, tc_version) values (new.id, (select latest_tc_version from global_settings));
   -- Don't use the avatar image for now
   -- insert into public.profiles (id, full_name, avatar_url)
   -- values (new.id, new.raw_user_meta_data->>'full_name', new.raw_user_meta_data->>'avatar_url');
