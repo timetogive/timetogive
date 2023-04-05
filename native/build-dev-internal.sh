3b4074c088c82361498766454abca13c5be91251
hoochani <james.allchin+dev@gmail.com>
2023-04-05 13:44:22 +0100

    Notes and more prod configuration

--- native/build-dev-internal.sh
+++ native/build-dev-internal.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo "-----------------------------------------------------------------------"
-echo "Building iOS app locally with production profile" 
+echo "Building iOS app locally with development profile" 
 echo "You must have the expo-cli installed, and XCode setup and configured"
 echo "Your device must be registered correctly with EAS"
 echo "The build will generated a .ipa file, which you can install on your device"
@@ -8,4 +8,4 @@ echo "To add to your device drag and drop through Finder with the phone connecte
 echo "Note you will need to trust the phone on your mac and your mac will need to trust the phone"
 echo "DON'T FORGET to enable developer mode in privacy and settings on your phone"
 echo "-----------------------------------------------------------------------"
-eas build --profile prod --platform ios --local
+eas build --profile dev-internal --platform ios --local
