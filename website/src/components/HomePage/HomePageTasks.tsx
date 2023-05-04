52cd20ae70b81557001f2e5407e8498158e9c240
hoochani <james.allchin+dev@gmail.com>
2023-05-04 11:17:20 +0100

    Website branding changes

--- website/src/components/HomePage/HomePageTasks.tsx
+++ website/src/components/HomePage/HomePageTasks.tsx
@@ -13,35 +13,27 @@ import {
 import { HiChevronRight } from 'react-icons/hi';
 
 const TaskPanel = ({
-  bgImage,
+  bgColor,
   bgPosition,
   children,
 }: {
-  bgImage: string;
+  bgColor: string;
   bgPosition?: string;
   children: React.ReactNode;
 }) => {
   return (
     <Box
       flex={1}
-      rounded={5}
+      rounded={8}
       h="400px"
-      bgColor="cyan.500"
-      bgImage={bgImage}
+      bgColor={bgColor}
       bgPosition={bgPosition || 'top'}
       bgSize="cover"
       position="relative"
       overflow="hidden"
+      p={8}
     >
-      <Box p={10} position="absolute" top="25" zIndex={1} h="150">
-        {children}
-      </Box>
-      <Box
-        boxSize="full"
-        position="absolute"
-        inset="0"
-        bgGradient="linear(to-b, rgba(32, 32, 32, 1), rgba(32, 32, 32, 0.2))"
-      />
+      {children}
     </Box>
   );
 };
@@ -74,6 +66,7 @@ export const HomePageTasks = () => {
           w={{ base: '100%', md: '4xl' }}
           maxW="100%"
           spacing={5}
+          color="#264553"
         >
           <Grid
             templateColumns={{
@@ -84,17 +77,27 @@ export const HomePageTasks = () => {
             w="full"
           >
             <GridItem>
-              <TaskPanel bgImage="https://images.unsplash.com/photo-1461532257246-777de18cd58b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fHZvbHVudGVlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60">
-                <VStack spacing={5} align="start">
-                  <Text color="white" fontSize="xl" fontWeight="bold">
+              <TaskPanel bgColor="#54B2D4">
+                <VStack spacing={4} alignItems="start">
+                  <Text
+                    fontSize="xl"
+                    fontWeight="bold"
+                    alignSelf="center"
+                  >
                     Helping out a neighbour
                   </Text>
-                  <Text color="white" fontSize="sm" fontWeight="bold">
+                  <Image
+                    alt="Helping out a neighbour"
+                    src="/task-icon-neighbours-dark.svg"
+                    h={24}
+                    alignSelf="center"
+                  />
+                  <Text fontSize="sm">
                     Examples : building a shed, helping put together
                     some Ikea furniture, lending someone a power tool,
                     fixing the wifi.
                   </Text>
-                  <Text color="white" fontSize="sm" fontWeight="bold">
+                  <Text fontSize="sm">
                     These favours are repaid in kind. By returning the
                     favour, pledging a small amount to charity, or by
                     simply paying it forward with another task for
@@ -104,17 +107,27 @@ export const HomePageTasks = () => {
               </TaskPanel>
             </GridItem>
             <GridItem>
-              <TaskPanel bgImage="https://images.unsplash.com/photo-1587040273238-9ba47c714796?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bmVpZ2hib3VyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60">
-                <VStack spacing={5} align="start">
-                  <Text color="white" fontSize="xl" fontWeight="bold">
+              <TaskPanel bgColor="#71BBA5">
+                <VStack spacing={4} alignItems="start">
+                  <Text
+                    fontSize="xl"
+                    fontWeight="bold"
+                    alignSelf="center"
+                  >
                     Assisting the vulnerable or elderly
                   </Text>
-                  <Text color="white" fontSize="sm" fontWeight="bold">
+                  <Image
+                    alt="Assisting the vulnerable or elderly"
+                    src="/task-icon-elderly-dark.svg"
+                    h={24}
+                    alignSelf="center"
+                  />
+                  <Text fontSize="sm">
                     Examples : dropping off food parcels, taking the
                     time have a coffee with someone lonely, offering a
                     lift to a hospital appointment.
                   </Text>
-                  <Text color="white" fontSize="sm" fontWeight="bold">
+                  <Text fontSize="sm">
                     Even tiny amounts of time can make a huge
                     difference.
                   </Text>
@@ -122,17 +135,27 @@ export const HomePageTasks = () => {
               </TaskPanel>
             </GridItem>
             <GridItem>
-              <TaskPanel bgImage="https://images.unsplash.com/photo-1674574124792-3be232f1957f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60">
-                <VStack spacing={5} align="start">
-                  <Text color="white" fontSize="xl" fontWeight="bold">
+              <TaskPanel bgColor="#F6CF56">
+                <VStack spacing={4} alignItems="start">
+                  <Text
+                    fontSize="xl"
+                    fontWeight="bold"
+                    alignSelf="center"
+                  >
                     For community groups and charities
                   </Text>
-                  <Text color="white" fontSize="sm" fontWeight="bold">
+                  <Image
+                    alt="For community groups and charities"
+                    src="/task-icon-charity-dark.svg"
+                    h={24}
+                    alignSelf="center"
+                  />
+                  <Text fontSize="sm">
                     Examples : sorting clothes in the high street
                     shop, helping to run a stall at a charity fair,
                     litter picking.
                   </Text>
-                  <Text color="white" fontSize="sm" fontWeight="bold">
+                  <Text fontSize="sm">
                     Traditional volunteering tasks, but smaller, and
                     only carried as and when you have the time without
                     long term commitments.
@@ -141,20 +164,27 @@ export const HomePageTasks = () => {
               </TaskPanel>
             </GridItem>
             <GridItem>
-              <TaskPanel
-                bgPosition="center"
-                bgImage="https://images.unsplash.com/photo-1599508704512-2f19efd1e35f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cXVlc3Rpb258ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
-              >
-                <VStack spacing={5} align="start">
-                  <Text color="white" fontSize="xl" fontWeight="bold">
-                    Answering a question or giving advice
+              <TaskPanel bgColor="#E27B60">
+                <VStack spacing={4} alignItems="start">
+                  <Text
+                    fontSize="xl"
+                    fontWeight="bold"
+                    alignSelf="center"
+                  >
+                    Answering questions or giving advice
                   </Text>
-                  <Text color="white" fontSize="sm" fontWeight="bold">
+                  <Image
+                    alt="Answering a question or giving advice"
+                    src="/task-icon-advice-dark.svg"
+                    h={24}
+                    alignSelf="center"
+                  />
+                  <Text fontSize="sm">
                     Examples : who&apos;s the best plumber in town?
                     who&apos;s hiring locally at the moment?
                     where&apos;s the best place to grab a coffee?
                   </Text>
-                  <Text color="white" fontSize="sm" fontWeight="bold">
+                  <Text fontSize="sm">
                     Tasks don&apos;t have to be physical things.
                     Answering questions and giving advice to your
                     local community adds real value and can be done
